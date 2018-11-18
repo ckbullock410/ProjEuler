e76537151c2b0ee3ce0c1f0d93b973714320b8bb
Chandler Bullock <ckbullock410@gmail.com>
2018-11-17 17:31:03 -0700

    Recursively solve

--- Euler2/Euler2.java
+++ Euler2/Euler2.java
@@ -2,7 +2,6 @@ public class Euler2{
 	public static void main(String args[]){
 		Euler2 e3= new Euler2();
 		e3.fibRecurse(0, 1, 0);
-		e3.fib(4000000);	
 	}
 
 	private void fibRecurse(int numOne, int numTwo, int sum){
