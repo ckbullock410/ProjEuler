b6638ae758c3bd2b989a30af7471fce28d519d1c
Chandler Bullock <ckbullock410@gmail.com>
2018-12-25 00:01:54 -0700

    problem 30 solved

---
+++ Euler30/Euler30.java
@@ -0,0 +1,31 @@
+public class Euler30 {
+     public static void main(String[] args){
+          int sum = 0;
+          //comput array of the quint powers
+          int[] myArray = new int[10];
+          for (int i = 0; i < 10; i++) {
+              myArray[i] = i*i*i*i*i;
+          }
+          // find max we need to calculate up to
+          int k = 100000;
+          while(myArray[9]*(Integer.toString(k)).length() > k){
+            k += 10;
+          }
+          // we now have a max that is no more than 9 greater than the entries we need to test
+
+         int hold = 0;
+         String holdr;
+         String temp;
+         for (int i = 11; i <= k; i++) {
+             hold = 0;
+             holdr = Integer.toString(i);
+             for (int j = 0; j < holdr.length(); j++) {
+                 temp = "" + holdr.charAt(j);
+                 hold += myArray[Integer.parseInt(temp)];
+                 if (hold > i) break;
+             }
+            if (hold == i) sum += hold;
+         }
+         System.out.println(sum);
+     }
+}
